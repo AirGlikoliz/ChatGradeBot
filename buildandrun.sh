047a01b8b207bc9ff6aab65dea41d2d808d53c36
Linnik.D <mrscreendark@gmail.com>
2025-02-03 11:24:24 +0300

    fix bui

--- buildandrun.sh
+++ buildandrun.sh
@@ -4,6 +4,8 @@ kill -9 "$(lsof -t -i :8080 -s TCP:LISTEN)"
 
 rm nohup.out
 
+rm build/libs/ChatGradeBot-0.0.1-SNAPSHOT.jar
+
 ./gradlew bootJar -x test
 
 nohup java -jar build/libs/ChatGradeBot-0.0.1-SNAPSHOT.jar &
